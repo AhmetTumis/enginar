32b90b9154eb060473f18eb5f8b449060e0bf35f
robobeebop <denizhankilercioglu@gmail.com>
2021-12-20 01:28:59 +0300

    Update SpriteTest.cpp

--- Enginar/SpriteTest.cpp
+++ Enginar/SpriteTest.cpp
@@ -14,12 +14,12 @@ int main()
         "assets/adventurer-idle-2-03.png",
     };
     
-    Layer* layerIDLE = playerAdventurer.createLayer("IDLE", imgs);
+    Layer* layerIDLE = playerAdventurer.createLayer("IDLE", idleSprites);
 
     playerAdventurer.setCurrentLayer("IDLE");
     
     
-    TextureNode* cur = sprite.currentLayer->head; // TODO: renderda hallolunasi icap olunasi
+    TextureNode* cur = playerAdventurer.currentLayer->head; // TODO: renderda hallolunasi icap olunasi
     while(cur != NULL){ // TODO: circular olacak
         cout << cur->texture << endl; // playerAdventurer.currentTexture
         cur = cur->next;
